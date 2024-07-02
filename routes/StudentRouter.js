8b23d6575328e40849417c131fd7a5483d95e09a
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-02 15:33:50 +0200

    Create StudentRouter.js

---
+++ routes/StudentRouter.js
@@ -0,0 +1,24 @@
+const { Router } = require('express');
+const StudentController = require('../controllers/StudentController');
+const AuthMid = require('../middlewares/AuthMid');
+
+const StudentRouter = Router();
+
+StudentRouter.post('/addAddress', AuthMid, StudentController.addStudentAddress);
+StudentRouter.put('/editAddress/:addressId', AuthMid, StudentController.editStudentAddress);
+StudentRouter.put('/editBasicInformation/:userId', AuthMid, StudentController.editStudentBasicInformation);
+StudentRouter.post('/addBasicEducation', AuthMid, StudentController.addBasicEducation);
+StudentRouter.put('/editBasicEducation/:basicEducationId', AuthMid, StudentController.editBasicEducation);
+StudentRouter.post('/addTertiaryEducation', AuthMid, StudentController.addTertiaryEducation);
+StudentRouter.put('/editTertiaryEducation/:tertiaryEducationId', AuthMid, StudentController.editTertiaryEducation);
+StudentRouter.delete('/deleteTertiaryEducation/:tertiaryEducationId', AuthMid, StudentController.deleteTertiaryEducation);
+StudentRouter.post('/addProfessionalSkill', AuthMid, StudentController.addProfessionalSkill);
+StudentRouter.put('/editProfessionalSkill/:professionalSkillId', AuthMid, StudentController.editProfessionalSkill);
+StudentRouter.delete('/deleteProfessionalSkill/:professionalSkillId', AuthMid, StudentController.deleteProfessionalSkill);
+StudentRouter.post('/addCertification', AuthMid, StudentController.addCertification);
+StudentRouter.put('/editCertificate', AuthMid, StudentController.editCertification);
+StudentRouter.delete('/deleteCertificate/:certificateId', AuthMid, StudentController.deleteCertification);
+StudentRouter.get('/downloadCertificate', StudentController.downloadCertificate)
+StudentRouter.post('/addDocument', AuthMid, StudentController.addDocument)
+
+module.exports = StudentRouter;
