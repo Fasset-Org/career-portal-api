94d7c9d444d6d16282eaef22494762bb534ad9f9
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-11 08:37:00 +0200

    Update 20240709135119-users.js

--- seeders/20240709135119-users.js
+++ seeders/20240709135119-users.js
@@ -1,8 +1,10 @@
-'use strict';
+"use strict";
+const { v4: uuidv4 } = require("uuid");
+const bcryptjs = require("bcryptjs");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,10 +13,30 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+
+    await queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          id: uuidv4(),
+          firstName: "ICT",
+          lastName: "Admin",
+          email: "devsupport@fasset.org.za",
+          password: await bcryptjs.hash(
+            "@Password123",
+            await bcryptjs.genSalt(10)
+          ),
+          userType: 'admin',
+          dateCreated: new Date(),
+          dateUpdated: new Date()
+        }
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
