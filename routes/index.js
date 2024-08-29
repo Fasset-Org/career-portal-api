1f3a56be380a1f643ad6f7c29a9ce981d5deee41
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-29 14:52:52 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const AuthRouter = require("./Authouter");
 const StudentRouter = require("./StudentRouter");
+const AdminRouter = require("./AdminRouter");
 
 const AppRouter = Router();
 
 AppRouter.use("/auth", AuthRouter);
 AppRouter.use("/student", StudentRouter);
+AppRouter.use('/admin', AdminRouter);
 
 module.exports = AppRouter;
