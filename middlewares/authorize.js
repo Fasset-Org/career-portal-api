73e3d64042681ec29e3b55a8e719c24af719354f
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-29 14:53:00 +0200

    Update authorize.js

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -1,13 +1,21 @@
+const { ApiError } = require("../utils/Response");
+
 exports.authorize = function (...userTypes) {
   return (req, res, next) => {
-    if (!userTypes.includes(req.user.userType)) {
-      next(
-        new ApiError(
-          `User role ${req.user.userType} is not authorized to access this route`,
-          403
-        )
-      );
+    console.log(req.user)
+    console.log(userTypes)
+    try {
+      if (!userTypes.includes(req.user.userType)) {
+        next(
+          new ApiError(
+            `User role ${req.user.userType} is not authorized to access this route`,
+            403
+          )
+        );
+      }
+      next();
+    } catch (e) {
+      console.log(e);
     }
-    next();
   };
 };
