f5905906ee1e96a02bbe53420390d139effb6157
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-29 14:52:55 +0200

    Update AdminRouter.js

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,15 +1,14 @@
 const { Router } = require("express");
 const AuthMid = require("../middlewares/AuthMid");
-const { addLearnerProgrammes } = require("../controllers/AdminController");
+const {
+  addLearnerProgrammes,
+  getAllStudents
+} = require("../controllers/AdminController");
 const { authorize } = require("../middlewares/authorize");
 
 const AdminRouter = Router();
 
-AdminRouter.post(
-  "/learnerProgramme",
-  AuthMid,
-  authorize("admin"),
-  addLearnerProgrammes
-);
+AdminRouter.post("/learnerProgramme", AuthMid, authorize("admin"), addLearnerProgrammes);
+AdminRouter.get("/getAllStudents", AuthMid, authorize("admin"), getAllStudents);
 
 module.exports = AdminRouter;
