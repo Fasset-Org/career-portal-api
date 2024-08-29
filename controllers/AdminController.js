3c78d82326996bdcc22c75c8c7c1b082b0a55c20
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-29 14:53:04 +0200

    Update AdminController.js

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,4 +1,15 @@
-const { LearnerProgramme } = require("../models");
+const {
+  LearnerProgramme,
+  User,
+  StudentInformation,
+  Address,
+  BasicEducation,
+  TertiaryEducation,
+  ProfessionalSkill,
+  Programme,
+  CertificateAndTraning,
+  Document
+} = require("../models");
 const { ApiResp } = require("../utils/Response");
 
 const AdminController = {
@@ -9,6 +20,73 @@ const AdminController = {
       return res
         .status(201)
         .json(ApiResp("Learner programme created successfully"));
+    } catch (e) {
+      console.log(e);
+      next(e);
+    }
+  },
+
+  getAllStudents: async (req, res, next) => {
+    try {
+      const students = await User.findAll({
+        where: { userType: "student" },
+        attributes: {
+          exclude: ["password"]
+        },
+        include: [
+          {
+            model: StudentInformation,
+            as: "studentInformation"
+          },
+          {
+            model: Address,
+            as: "studentAddress",
+            attributes: {
+              exclude: [
+                "id",
+                "dateCreated",
+                "dateUpdated",
+                "userId",
+                "completed"
+              ]
+            }
+          },
+          {
+            model: BasicEducation,
+            as: "basicEducation"
+          },
+          {
+            model: TertiaryEducation,
+            as: "tertiaryEducation"
+          },
+          {
+            model: ProfessionalSkill,
+            as: "skills"
+          },
+          {
+            model: LearnerProgramme,
+            as: "studentProgrammes",
+            include: [
+              {
+                model: Programme,
+                as: "programmes"
+              }
+            ]
+          },
+          {
+            model: CertificateAndTraning,
+            as: "certificates"
+          },
+          {
+            model: Document,
+            as: "attachments"
+          }
+        ]
+      });
+
+      return res
+        .status(200)
+        .json(ApiResp("All students fetched", "students", students));
     } catch (e) {
       console.log(e);
       next(e);
