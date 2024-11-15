b64b1ba1cb803f632bae828a1139dacdedc81053
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-11-15 11:27:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ const app = express();
 dotenv.config({ path: `${__dirname}/config/config.env` });
 
 // middlewares
+app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(morgan("method :url :status :res[content-length] - :response-time ms"));
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
